a20bd5de55752f1b2844093efeaf0d68b25cc20e
Paiva <icaropaiva0112@gmail.com>
2024-05-04 18:26:24 -0300

    atualização de funcionalidades

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ function logar(){
 
   if(login != 'admin' || senha != '1234'){
     document.getElementById('mensagem-erro').style.display = "block"
+    document.getElementById('campos-erro').style.display = "none"
     
   }
   else{
     document.getElementById('mensagem-erro').style.display = "none"
     location.href = "sucesso.html";  
 }
+
+  if (login == '' || senha ==''){
+    document.getElementById('campos-erro').style.display = "block"
+    document.getElementById('mensagem-erro').style.display = "none"
+  }
+
 
 }
   
